00d66527c94b414a2197b7c2be221c7c8a332e67
Alan Lavender <alanlavendersmv@gmail.com>
2023-05-05 10:32:29 +0100

    Add files via upload

--- 04_conditionals_survey.py
+++ 04_conditionals_survey.py
@@ -15,5 +15,46 @@ if over 60,
 '''
 
 print("SOCIAL MEDIA SURVEY: please enter your age >>>")
-age = input()
-age = input("SOCIAL MEDIA SURVEY: please enter your age >>>")
+age = int(input())#no args
+# age = input("SOCIAL MEDIA SURVEY: please enter your age >>>")#1 arg
+# # somewhere in sytem code (built_ins)
+# def input():
+#     #TODO
+#     pass
+# # may or may not take an arg
+# def input(message):
+#     #TODO
+#     pass
+
+my_socials = ""
+print("Do you use either of the following two social platforms: type Y for yes, any character for no")
+if age < 21:
+    print("TikTok? Y/N")
+    answer = input()
+    if answer.upper() == "Y":
+        my_socials += "TikTok, "
+    print("SnapChat? Y/N")
+    answer = input()
+    if answer.upper() == "Y":
+        my_socials += "SnapChat, "
+if age >= 21 and age <= 60:
+    print("Facebook? Y/N")
+    answer = input()
+    if answer.upper() == "Y":
+        my_socials += "Facebook, "
+    print("Twitter? Y/N")
+    answer = input()
+    if answer.upper() == "Y":
+        my_socials += "Twitter, "
+if age > 60:
+    print("MySpace? Y/N")
+    answer = input()
+    if answer.upper() == "Y":
+        my_socials += "MySpace, "
+    print("Reddit? Y/N")
+    answer = input()
+    if answer.upper() == "Y":
+        my_socials += "Reddit, "
+
+# print(my_socials)#testing
+print(f'You entered age {age} and use {my_socials}')
